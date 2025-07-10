4dc98cd385a74891c8e8136967749071c864d089
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-07-10 16:48:09 +0200

    Add PreLogChatEvent

---
+++ src/main/java/net/coreprotect/event/chat/PreLogChatEvent.java
@@ -0,0 +1,83 @@
+package net.coreprotect.event.chat;
+
+import com.google.common.base.Preconditions;
+import io.papermc.paper.event.player.AbstractChatEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Called after a chat event is received and before it is queued to be logged, allowing for the resulting logged message to be modified.
+ * <p>
+ * This event is not called if player chat logging is not enabled.
+ */
+public class PreLogChatEvent extends Event implements Cancellable {
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    private final AbstractChatEvent event;
+    private boolean chatCancelled;
+    private String messageToLog;
+    private boolean cancelled;
+
+    @ApiStatus.Internal
+    public PreLogChatEvent(AbstractChatEvent event, String messageToLog) {
+        this.event = event;
+        this.messageToLog = messageToLog;
+        this.chatCancelled = event.isCancelled();
+    }
+
+    @NotNull
+    public String getMessageToLog() {
+        return messageToLog;
+    }
+
+    public void setMessageToLog(@NotNull String messageToLog) {
+        Preconditions.checkArgument(messageToLog != null, "messageToLog may not be null");
+        this.messageToLog = messageToLog;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    /**
+     * Whether this message will be logged as cancelled or not, defaults to the cancelled state of the event. Using this method does not affect the cancellation state of the chat event, nor is the state of the event re-checked after.
+     *
+     * @return Whether this event is logged as a cancelled event or not.
+     */
+    public boolean isChatCancelled() {
+        return this.chatCancelled;
+    }
+
+    public void setChatCancelled(boolean chatCancelled) {
+        this.chatCancelled = chatCancelled;
+    }
+
+    @NotNull
+    public AbstractChatEvent getChatEvent() {
+        return this.event;
+    }
+
+    @NotNull
+    public Player getPlayer() {
+        return this.event.getPlayer();
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+}
--- src/main/java/net/coreprotect/listener/player/PlayerChatListener.java
+++ src/main/java/net/coreprotect/listener/player/PlayerChatListener.java
@@ -1,6 +1,7 @@
 package net.coreprotect.listener.player;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
+import net.coreprotect.event.chat.PreLogChatEvent;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,15 +16,20 @@ public final class PlayerChatListener extends Queue implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerChat(AsyncChatEvent event) {
         Player player = event.getPlayer();
-        if (event.isCancelled() && !Config.getConfig(player.getWorld()).LOG_CANCELLED_CHAT) {
+
+        String message = PlainTextComponentSerializer.plainText().serialize(event.message());
+
+        final PreLogChatEvent preEvent = new PreLogChatEvent(event, message);
+
+        if (message.startsWith("/") || !Config.getConfig(player.getWorld()).PLAYER_MESSAGES) {
+            preEvent.setCancelled(true);
+        }
+
+        if (!preEvent.callEvent() || (preEvent.isChatCancelled() && !Config.getConfig(player.getWorld()).LOG_CANCELLED_CHAT)) {
             return;
         }
 
-        final String message = PlainTextComponentSerializer.plainText().serialize(event.message());
-
-        if (!message.startsWith("/") && Config.getConfig(player.getWorld()).PLAYER_MESSAGES) {
-            long timestamp = System.currentTimeMillis() / 1000L;
-            Queue.queuePlayerChat(player, message, timestamp);
-        }
+        long timestamp = System.currentTimeMillis() / 1000L;
+        Queue.queuePlayerChat(player, preEvent.getMessageToLog(), timestamp);
     }
 }
